e974abec62ea4cf0bc458010e05e21734a09e612
Neboer <rubinposter@gmail.com>
2024-12-05 10:24:18 +0800

    将缓存管理放入文件夹中。

---
+++ cache-management/cache-target.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+SYSTEM_CACHE_DIRS=(
+    root.x86_64/var/cache
+)
+
+USER_CACHE_DIRS=(
+    root.x86_64/home/archlinuxus/.cache
+)
+
+ALL_CACHE_DIRS+=( "${SYSTEM_CACHE_DIRS[@]}" "${USER_CACHE_DIRS[@]}" )
--- cache-management/perpare-cache.sh
+++ cache-management/perpare-cache.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
 # create-cache
-CACHE_DIRS=(
-    root.x86_64/var/cache
-    root.x86_64/home/archlinuxus/.cache
-)
+# need rsync to run the script.
+
+source cache-management/cache-target.sh
 
 mkdir -p cache_backups
 for i in ${CACHE_DIRS[@]}; do
