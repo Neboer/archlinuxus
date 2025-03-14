246991d6b24a16b2f289352c89ebba3cd558fab1
Neboer <rubinposter@gmail.com>
2025-03-14 23:38:17 +0800

    修复脚本执行路径的问题

--- prepare-arch-env/main.sh
+++ prepare-arch-env/main.sh
@@ -4,8 +4,8 @@ USERNAME=archlinuxus
 PROJ_DIR=/root/archlinuxus-scripts
 
 cd "$PROJ_DIR"
-source prepare-arch-env/prepare-mirrors.sh
-source prepare-arch-env/prepare-software.sh
-source prepare-arch-env/prepare-user.sh
-source prepare-arch-env/prepare-yay.sh
-source prepare-arch-env/prepare-makepkg.sh
+source "$PROJ_DIR/prepare-arch-env/prepare-mirrors.sh"
+source "$PROJ_DIR/prepare-arch-env/prepare-software.sh"
+source "$PROJ_DIR/prepare-arch-env/prepare-user.sh"
+source "$PROJ_DIR/prepare-arch-env/prepare-yay.sh"
+source "$PROJ_DIR/prepare-arch-env/prepare-makepkg.sh"
